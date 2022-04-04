20095a6b3f30c7d35b30b8302aa4201458fad30d
Mckenna Preston <Mckenna_Preston1@baylor.edu>
2022-04-04 14:59:18 -0500

    class 4/4

--- webscraping-tradingview.py
+++ webscraping-tradingview.py
@@ -43,3 +43,22 @@ for company in companies[:5]:
 #Limit = find with limit of 1
 #keyword: allText = Obj.find(id="title",class="text")
 
+stock_table = soup.findAll('tbody')
+
+stock_table = stock_table[0]
+
+rows = stock_table.findAll('tr')
+
+for row in rows[:5]:
+    cols = row.findAll('td')
+    name = cols[0].find("span", attrs={'class':'tv-screener__description'})
+    name = name.text.strip()
+    price = float(cols[1].text)
+    change_text = cols[2].text
+    change_float = float(cols[2].text.strip('%'))
+
+    s_price = round(price/(1+(change_float/100)),2)
+    print(f"Company Name: {name}")
+    print(f"Current Price: {price}")
+    print(f"% change: {change_text}")
+    print(f"Starting Price: {s_price}")
